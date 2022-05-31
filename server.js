b779c4aa34df34c83409195a6296e2520b49f223
José Victor Gaspari <josevictorgaspari@gmail.com>
2022-05-31 08:00:39 -0300

    feat: add server to run build

---
+++ server.js
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-undef */
+const express= require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3030;
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on localhost:${PORT}`);
+});
